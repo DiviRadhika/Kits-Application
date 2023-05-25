0654e364af9336827a7c18569277f66af9e2bbfb
Sunil <sunilkumar.i@roboxaservices.com>
2023-05-25 09:37:34 +0530

    latest changes are pushed

--- src/app/Services/SITE/site.service.ts
+++ src/app/Services/SITE/site.service.ts
@@ -9,7 +9,7 @@ export class SiteService {
 
   constructor(private _httpClient:HttpClient) { }
   getSites():Observable<any>{
-    return this._httpClient.get<any>("http://34.100.227.119:5001/api/site_data");
+    return this._httpClient.get<any>("http://34.100.227.119:5001/api/sites_data");
   }
   CreateSiteDetails(data:string):Observable<any>{
     return this._httpClient.post("http://34.100.227.119:5001/api/site_data",data);
