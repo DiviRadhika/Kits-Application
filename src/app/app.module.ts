0847db5558ed1e2ebe830bb73dd11a7e58bb2194
narendraB056 <narendraB056>
2023-05-29 14:43:41 +0530

    Changed admin routing and module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
-import { NgModule } from '@angular/core';
+import { ApplicationModule, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { ApplicationadminRoutingModule } from './applicationadmin/applicationadmin-routing.module';
-import { ApplicationadminModule } from './applicationadmin/applicationadmin.module';
+
 import { CROModule } from './cro/cro.module';
 import { SponsorModule } from './sponsor/sponsor.module';
 import { HomeComponent } from './home/home.component';
@@ -14,7 +13,11 @@ import { CentralLabModule } from './central-lab/central-lab.module';
 import { SiteModule } from './site/site.module';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ApplicationadminModule } from './applicationadmin/applicationadmin.module';
+
+
+
 
 @NgModule({
   declarations: [
@@ -28,11 +31,12 @@ import { ReactiveFormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ApplicationadminModule,
     CROModule,
     SponsorModule,
     CentralLabModule,SiteModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    ApplicationadminModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
