d21f9d3f08339f0392ce2b0f6366761ec9b19e78
lakshmi sindhuja <sindhujad@roboxaservices.com>
2023-08-22 19:27:55 +0530

    'extension'

--- src/app/central-lab/central-lab-routing.module.ts
+++ src/app/central-lab/central-lab-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { KitPreprationComponent } from './kit-Prepration/kit-Prepration.component';
+import { KitPreprationComponent } from './kit-prepration/kit-prepration.component';
 import { KitVerificationComponent } from './kit-verification/kit-verification.component';
 import { SampleAcknowledgementComponent } from './sample-acknowledgement/sample-acknowledgement.component';
 import { UploadResultsComponent } from '../central/upload-results/upload-results.component';
 import { KitDistributionComponent } from './kit-distribution/kit-distribution.component';
 import { SampleReportsComponent } from './sample-acknowledgement/sample-reports/sample-reports.component';
-import { PreprationGridComponent } from './Prepration-grid/Prepration-grid.component';
-import { KitPreprationEditComponent } from './kit-Prepration-edit/kit-Prepration-edit.component';
+import { PreprationGridComponent } from './prepration-grid/prepration-grid.component';
+import { KitPreprationEditComponent } from './kit-prepration-edit/kit-prepration-edit.component';
 
 const routes: Routes = [
   {path:'kitPrepration/:id/:mode', component:KitPreprationComponent},
@@ -15,8 +15,8 @@ const routes: Routes = [
   {path:'kitDistribution', component:KitDistributionComponent},
   {path:'sampleAcknowledgement', component:SampleAcknowledgementComponent},
   {path:'sampleReports', component:SampleReportsComponent},
-  {path:'kitPreprationGrid', component:PreprationGridComponent},
-  {path:'kitPreprationedit/:id', component:KitPreprationEditComponent},
+  {path:'kitPreparationGrid', component:PreprationGridComponent},
+  {path:'kitpreprationedit/:id', component:KitPreprationEditComponent},
   
 
 
